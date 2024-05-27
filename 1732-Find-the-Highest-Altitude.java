1b6307f356626b6ae51c40d3a46149d35376a6c3
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-27 23:26:02 +0530

    Create 1732-Find-the-Highest-Altitude.java
    Runtime - 0ms, Space consumed - 41.18MB

---
+++ 1732-Find-the-Highest-Altitude.java
@@ -0,0 +1,16 @@
+Link - https://leetcode.com/problems/find-the-highest-altitude/description/
+class Solution {
+    public int largestAltitude(int[] gain) {
+        int max = 0;
+        int currentAltitude = 0;
+
+        for (int i = 0; i < gain.length; i++) {
+            currentAltitude += gain[i];
+            if (currentAltitude > max) {
+                max = currentAltitude;
+            }
+        }
+
+        return max;
+    }
+}
