722ab02207e5d437c8ab14cd6bb6e6390c58cb2e
codershrutik <72141758+codershrutik@users.noreply.github.com>
2024-05-29 12:32:40 +0000

    Modified, accepts all test cases

--- 1295-Finding-Even-Number-Of-Digits.java
+++ 1295-Finding-Even-Number-Of-Digits.java
@@ -1,21 +1,19 @@
 //Link to problem - https://leetcode.com/problems/find-numbers-with-even-number-of-digits/description/
-import java.util.Arrays;
-
 class Solution {
     public int findNumbers(int[] nums) {
 
         int count = 0;
-        int countn = 0;
-        
-        for(int i=0;i<nums.length;i++){
-            while (nums[i] != 0) {
-                nums[i] /= 10;
+       
+        for(int num: nums){
+            int countn = 0;
+            int n = num;
+            while (n != 0) {
+                n /= 10;
                 ++countn;
             }
             if(countn % 2 == 0){
                 count++;
             }
-
         }
         return count;
     }
